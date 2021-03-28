cd8959431f730cc9f8cbfa478799f21ecfaf1e89
Barnabas Molnar <hanta911@gmail.com>
2021-03-28 01:30:19 +0100

    async await

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const renderCountry = function(data, className = '') {
             </div>
         </article> `
         countriesContainer.insertAdjacentHTML('beforeend', html);
-        // countriesContainer.style.opacity = 1;
+        countriesContainer.style.opacity = 1;
     }
     // const getCountryAndNeighbour = function(country) {
 
@@ -240,101 +240,77 @@ const getPosition = function() {
 
 // getPosition().then(pos => console.log(pos))
 
-const whereIam = function() {
+// const whereIam = function() {
 
 
-    getPosition().then(pos => {
-            const { latitude: lat, longitude: lng } = pos.coords;
-            return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-        })
-        .then((response) => {
-            console.log(response)
-            if (!response.ok) throw new Error("Max 3 request per second")
-            return response.json()
-        }).then(data => {
-            console.log(data)
-            const city = data.city
-            console.log(`You are currently in ${city} in ${data.country} `)
-            return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`)
-        }).then(response => {
-            if (!response.ok) throw new Error(`Country doesn't exists ${response.status}`);
-            return response.json();
-        }).then(data => {
+//     getPosition().then(pos => {
+//             const { latitude: lat, longitude: lng } = pos.coords;
+//             return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//         })
+//         .then((response) => {
+//             console.log(response)
+//             if (!response.ok) throw new Error("Max 3 request per second")
+//             return response.json()
+//         }).then(data => {
+//             console.log(data)
+//             const city = data.city
+//             console.log(`You are currently in ${city} in ${data.country} `)
+//             return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`)
+//         }).then(response => {
+//             if (!response.ok) throw new Error(`Country doesn't exists ${response.status}`);
+//             return response.json();
+//         }).then(data => {
 
-            renderCountry(data[0])
-            countriesContainer.style.opacity = 1;
-        })
-        .catch(err => {
-            console.log(`${err.message}🍕 `)
-        }).finally(() => {
-            console.log("finally")
-        })
-}
+//             renderCountry(data[0])
+//             countriesContainer.style.opacity = 1;
+//         })
+//         .catch(err => {
+//             console.log(`${err.message}🍕 `)
+//         }).finally(() => {
+//             console.log("finally")
+//         })
+// }
 
-btn.addEventListener('click', whereIam);
-///////////////////////////////////////
-// Coding Challenge #2
+// btn.addEventListener('click', whereIam);
 
-/*
-Build the image loading functionality that I just showed you on the screen.
+const whereAmI = async function(country) {
 
-Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+    try { // Geolocation
+        const pos = await getPosition();
+        const { latitude: lat, longitude: lng } = pos.coords;
+        // Reverse geocoding
+        const resGeo = await fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+        if (!resGeo.ok) throw new Error('Problem getting location date ')
+        const dataGeo = await resGeo.json()
+            // Country data
+        const res = await fetch(`https://restcountries.eu/rest/v2/name/${dataGeo.country}`)
+        if (!res.ok) throw new Error('Problem getting country ')
+        const data = await res.json();
+        renderCountry(data[0])
+        return `You are in ${dataGeo.city}, ${dataGeo.country}`
+    } catch (err) {
+        console.error(`${err}💩`);
+        renderError(`💣 ${err.message}`)
 
-PART 1
-1. Create a function 'createImage' which receives imgPath as an input. This function returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
-
-If this part is too tricky for you, just watch the first part of the solution.
-
-PART 2
-2. Comsume the promise using .then and also add an error handler;
-3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
-4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
-5. After the second image has loaded, pause execution for 2 seconds again;
-6. After the 2 seconds have passed, hide the current image.
-
-TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
-
-GOOD LUCK 😀
-*/
-const wait = function(seconds) {
-    return new Promise(function(resolve) {
-        setTimeout(resolve, seconds * 1000)
-    });
+        // Reject promise returned from async functions 
+        throw err;
+    }
 };
-const div = document.querySelector('.images')
 
-const createImage = function(imgPath) {
-    return new Promise(function(resolve, reject) {
-        const img = document.createElement('img')
-        img.src = imgPath
+console.log('1: will get location');
+// const city = whereAmI();
+// console.log(city)
+// whereAmI()
+//     .then(city => console.log(`2: ${city}😎`))
+//     .catch(err => console.error(`2: ${err}💥`))
+//     .finally(() => console.log('3: Finished getting location'))
 
-        img.addEventListener('load', function() {
-            div.append(img)
-            resolve(img)
-        })
-        img.addEventListener('error', function() {
-            reject(new Error('Something went wrong 💩'))
-        })
-    })
-};
-let currentImg
-createImage('img/img-1.jpg')
-    .then(img => {
-        currentImg = img
-        console.log('image 1 is loaded')
-        return wait(2)
-    })
-    .then(() => {
-        currentImg.style.display = 'none'
-        console.log("2sec passed")
-        return createImage('img/img-2.jpg')
-    })
-    .then(img => {
-        currentImg = img
-        console.log('image 2 is loaded')
-        return wait(2)
-    }).then(() => {
-        currentImg.style.display = 'none'
-        console.log("2picture is gone too")
-    })
-    .catch(err => console.error(err))
+(async function() {
+    try {
+        const city = await whereAmI()
+        console.log(`2: ${city}😎`);
+    } catch (err) {
+        console.error(`2: ${err}💥`);
+    }
+    console.log('3: Finished getting location')
+})();
